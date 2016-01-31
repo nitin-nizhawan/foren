8d4e364b3bbe080a9842169547a3923c0826903f
Nitin Nizhawan <nitin.nizhawan@gmail.com>
2016-02-01 01:20:18 +0530

    Add a copy of generated js file in app folder

--- c.sh
+++ c.sh
@@ -1,2 +1,3 @@
 mvn clean install
 scalajsld -d target -o target/nitin-nizhawan-foren.js  target/classes ~/.m2/repository/org/scala-js/scala-parser-combinators_sjs0.6_2.11/1.0.2/scala-parser-combinators_sjs0.6_2.11-1.0.2.jar
+cp target/nitin-nizhawan-foren.js app/
